9224e10db013093841d37eef12fc0c68e1e8adb6
anwarrsyd <anwarrosyidi.5@gmail.com>
2017-02-24 15:51:27 +0700

    Sudah bisa data kondisi cuaca dari server BMKG

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use DB;
+use SimpleXMLElement;
 
 use Illuminate\Http\Request;
 
@@ -40,7 +41,8 @@ class HomeController extends Controller
             $y2 = $this->finalLatitude($y3, $y1);
             $array[$i] = DB::SELECT("SELECT * FROM pgr_aStarFromAtoBviaC_line('ways', ".$x3.",".$y3.",".$x2.",".$y2.",".$x1.",".$y1.")");
         }
-        return json_encode($array);
+        $ok=array_values($array);
+        return json_encode($ok);
     }
 
     function randomFloat($min = 0, $max = 1) {
@@ -98,5 +100,55 @@ class HomeController extends Controller
 
     return $batas_baru;
   }
+
+
+    public function kondisicuaca(){
+        // $str = file_get_contents('http://localhost/a');
+        $str=$this->getRoute(112.7991422,-7.2817807,112.723723,-7.336683);
+        // dd($str);
+        $coba=json_decode($str);
+        // dd($coba);
+        foreach ($coba['0'] as $key => $value) {
+           $x=$value->x;
+           $y=$value->y;
+           
+            if($x!==null){
+                   $query=DB::select("SELECT * FROM datapos  ORDER BY the_geom <-> ST_GeometryFromText('POINT(".$x."  ".$y.")',4326)  LIMIT 1 ");
+                    //     $query=DB::select("select *, sqrt(power( (6371*cos(datapos.ydesimal)*cos(datapos.xdesimal))- (6371*cos(".$y.")*cos(".$x.")),2)+
+                    // power( (6371*cos(datapos.ydesimal)*sin(datapos.xdesimal))-(6371*cos(".$y.")*sin(".$x.")),2)) as jarak from datapos 
+                    // order by jarak limit 1");
+
+                   foreach ($query as $key => $hasil) {
+                    $kategoricuaca=DB::table('rekaman')->select('kategori')->where('idpos','=',$hasil->idpos)->get();
+                    foreach ($kategoricuaca as $key => $mantap) {
+                                        echo $hasil->namapos;
+                                        echo " ";
+                                        echo $mantap->kategori;
+                                        echo "<br>";
+                                    }                
+
+                }
+            }
+           
+        }
+      
+    }
+
+    public function cobaxml(){
+        $xml=file_get_contents("http://110.139.67.15/sby/rekaman_tenminute.xml");
+        $data=new SimpleXMLElement($xml);
+        foreach ($data->rekaman as $key ) {
+            DB::table('rekaman')->insert(
+                    ['idrekaman'=>$key->idrekaman,
+                     'idpos'=>$key->idpos,
+                     'tipe'=>$key->tipe,
+                     'curah'=>$key->curah,
+                     'kategori'=>$key->kategori
+                    ]
+                );
+        }
+
+    }
+
 }
  
